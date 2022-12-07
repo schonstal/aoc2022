6b11fa8b82807e72287e14e748d1efe04ca46cf6
Josh Schonstal <josh@schonstal.org>
2022-12-06 18:12:05 -0800

    Accidentally thought part 2 was day 2.

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,22 +1,29 @@
 use std::fs;
+use std::collections::BinaryHeap;
 
 fn main() {
     let file_contents = fs::read_to_string("list.txt").expect("failed to read list.txt");
     let lines = file_contents.split("\n");
 
     let mut current = 0;
-    let mut max = 0;
+    let mut results = BinaryHeap::new(); 
 
     for line in lines {
         match line.parse::<i32>() {
             Ok(value) => current += value,
-            Err(_) => current = 0 //sorry not sorry
-        }
-
-        if current > max {
-            max = current;
+            Err(_) => {
+                results.push(current);
+                current = 0;
+            }
         }
     }
     
-    println!("answer: {}", max);
+    let mut sum = 0;
+    for _ in 0..3 {
+        let value = results.pop().unwrap();
+        sum += value;
+        println!("{:?}", value);
+    }
+
+    println!("sum: {:?}", sum);
 }
